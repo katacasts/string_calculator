df728f3bea3097e0709fd9d7f82b371d3217e194
Corey Haines <coreyhaines@gmail.com>
2009-12-04 22:12:07 -0500

    Reference Implementation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,23 @@
 module StringCalculator
+  def add
+    return 0 if empty?
+    raise_if_negatives
+    return digits.inject {|sum, x| sum + x }
+  end
+
+  def raise_if_negatives
+    raise "Negatives not allowed: #{negatives.join(", ")}" unless negatives.empty?
+  end
+
+  def negatives
+    @negatives ||= digits.select {|x| x<0}
+  end
+
+  def digits
+    gsub("\n",delimiter).split(delimiter).map{|x| x.to_i}
+  end
+
+  def delimiter
+    @delimiter ||= self[0,2] == '//' ? delimiter = self[2,1] : ','
+  end
 end
