0fa15a7d412e8b4c631713ae0d6e1af48a9f43c7
Ryan Castillo <rmcastil@gmail.com>
2012-07-12 08:04:33 -0400

    Remove RSpec deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'string_calculator'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   
 end
